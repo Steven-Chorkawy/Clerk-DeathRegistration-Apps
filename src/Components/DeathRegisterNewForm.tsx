1f7d15890238796a333cf5669e0760c69b480cc0
Steven Chorkawy <schorkawy@clarington.net>
2023-08-31 15:34:20 -0400

    Trying to use PnP Dynamic Form.

--- src/Components/DeathRegisterNewForm.tsx
+++ src/Components/DeathRegisterNewForm.tsx
@@ -1,6 +1,10 @@
-import { TextField } from '@fluentui/react';
 import * as React from 'react';
 import { IDeathRegisterFormProps } from '../extensions/deathRegisterForm/components/DeathRegisterForm';
+import { DynamicForm } from "@pnp/spfx-controls-react/lib/DynamicForm";
+import { IDynamicFieldProps } from '@pnp/spfx-controls-react/lib/controls/dynamicForm/dynamicField';
+import { ActionButton } from '@fluentui/react';
+import { DefaultEffects } from '@fluentui/react';
+import { DEATH_REGISTRATION_CONTENT_TYPE_ID, DEATH_REGISTRATION_LIST_ID } from '../MyHelperMethods/MyHelperMethods';
 
 export interface IDeathRegisterNewFormProps extends IDeathRegisterFormProps {
 }
@@ -12,12 +16,32 @@ export default class DeathRegisterNewForm extends React.Component<IDeathRegister
 
     public render(): React.ReactElement<{}> {
         return (
-            <div style={{ padding: '50px' }}>
-                <div>
-                    <TextField label="Folder Name" id={'myTestName'} />
-                    <br />
-                    {/* <PrimaryButton text="Click Me" onClick={this._onSave} /> */}
-                </div>
+            <div style={{ margin: '10px', padding: '10px' }}>
+                <DynamicForm
+                    context={this.props.context as any}
+                    listId={DEATH_REGISTRATION_LIST_ID}
+                    contentTypeId={DEATH_REGISTRATION_CONTENT_TYPE_ID}
+                    //listItemId={1}
+                    onCancelled={() => { console.log('Cancelled') }}
+                    onBeforeSubmit={async (listItem) => {
+                        console.log('before submit');
+                        console.log(listItem);
+                        return true;
+                    }}
+                    onSubmitError={(listItem, error) => { alert(error.message); }}
+                    onSubmitted={async (listItemData) => { console.log(listItemData); }}
+                    returnListItemInstanceOnSubmit={true}
+                    hiddenFields={['Year']}
+                    fieldOverrides={{
+                        "RegistrationNumber": (fieldProperties: IDynamicFieldProps) =>
+                            <div style={{ boxShadow: DefaultEffects.elevation8 }}>
+                                <ActionButton iconProps={{ iconName: 'NumberSymbol' }} onClick={() => { alert('TODO, get a number.'); }}>
+                                    Create Next Registration Number
+                                </ActionButton>
+                            </div>,
+                    }}
+                >
+                </DynamicForm>
             </div>
         );
     }
