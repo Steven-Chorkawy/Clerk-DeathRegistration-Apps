df4dc3540c40187f11677aaf835fb66f4bfc55b7
Steven Chorkawy <schorkawy@clarington.net>
2023-08-31 15:34:04 -0400

    adding constants.

--- src/MyHelperMethods/MyHelperMethods.tsx
+++ src/MyHelperMethods/MyHelperMethods.tsx
@@ -2,6 +2,10 @@ import { SPFI, SPFx, spfi } from "@pnp/sp";
 import { WebPartContext } from "@microsoft/sp-webpart-base";
 import { FormCustomizerContext, ListViewCommandSetContext } from "@microsoft/sp-listview-extensibility";
 
+//#region CONST
+export const DEATH_REGISTRATION_CONTENT_TYPE_ID = "0x0100DA81DCD717B72D499724C0023271F50C00D36BB2C588D851418CF8CEDFA4ED7036";
+export const DEATH_REGISTRATION_LIST_ID = "05310795-3642-4f5f-b71f-47254e1108be";
+//#endregion
 
 let _sp: SPFI = null;
 
@@ -11,3 +15,4 @@ export const getSP = (context?: WebPartContext | ListViewCommandSetContext | For
     }
     return _sp;
 };
+
