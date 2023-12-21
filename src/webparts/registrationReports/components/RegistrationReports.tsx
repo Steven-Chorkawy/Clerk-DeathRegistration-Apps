6acaf582911ba0c29f2e369e9edad62495083007
Steven Chorkawy <schorkawy@clarington.net>
2023-12-21 11:29:01 -0500

    IT IS WORKING!

--- src/webparts/registrationReports/components/RegistrationReports.tsx
+++ src/webparts/registrationReports/components/RegistrationReports.tsx
@@ -1,5 +1,4 @@
-import { DatePicker, Dropdown, IDropdownOption, Stack } from '@fluentui/react';
-import { PrimaryButton } from 'office-ui-fabric-react';
+import { DatePicker, DetailsList, Dropdown, IColumn, IDropdownOption, PrimaryButton, SelectionMode, Stack } from '@fluentui/react';
 import * as React from 'react';
 import PackageSolutionVersion from '../../../Components/PackageSolutionVersion';
 import { GetRegistrationReport, getSP } from '../../../MyHelperMethods/MyHelperMethods';
@@ -25,6 +24,176 @@ export default class RegistrationReports extends React.Component<any, IRegistrat
   }
 
   public render(): React.ReactElement<any> {
+    const DETAIL_LIST_DEATH_REG_COLUMNS: IColumn[] = [
+      {
+        key: 'column1',
+        name: 'Deceased',
+        fieldName: 'Title',
+        minWidth: 30,
+        maxWidth: 100,
+        isResizable: true,
+      },
+      {
+        key: 'column2',
+        name: 'Death Date',
+        fieldName: 'DateOfDeath',
+        minWidth: 30,
+        isResizable: true,
+        onRender: (item: IStillAndDeathRegisterListItem) => <span>{new Date(item.DateOfDeath).toLocaleDateString()}</span>
+      },
+      {
+        key: 'column3',
+        name: 'Age',
+        fieldName: 'Age',
+        minWidth: 30,
+        maxWidth: 60,
+        isResizable: true,
+      },
+      {
+        key: 'column4',
+        name: 'Reg Date',
+        fieldName: 'RegistrationDate',
+        minWidth: 30,
+        maxWidth: 60,
+        isResizable: true,
+        onRender: (item: IStillAndDeathRegisterListItem) => <span>{new Date(item.RegistrationDate).toLocaleDateString()}</span>
+      },
+      {
+        key: 'column5',
+        name: 'Reg Num',
+        fieldName: 'RegistrationNumber',
+        minWidth: 30,
+        isResizable: true,
+      },
+      {
+        key: 'column6',
+        name: 'Sex',
+        fieldName: 'Sex',
+        minWidth: 30,
+        isResizable: true,
+      },
+      {
+        key: 'column7',
+        name: 'Location',
+        fieldName: 'DeathLocation',
+        minWidth: 30,
+        isResizable: true,
+      },
+      {
+        key: 'column8',
+        name: 'Cause of Death',
+        fieldName: 'Cause',
+        minWidth: 30,
+        isResizable: true,
+      },
+      {
+        key: 'column9',
+        name: "Doctor",
+        fieldName: "DoctorsInformation",
+        minWidth: 30,
+        isResizable: true,
+      },
+      {
+        key: 'column10',
+        name: "Informant",
+        fieldName: "InformantsInformation",
+        minWidth: 30,
+        isResizable: true,
+      },
+      {
+        key: 'column11',
+        name: "Funeral Home",
+        fieldName: "FuneralHome",
+        minWidth: 30,
+        isResizable: true,
+        onRender: (item: IStillAndDeathRegisterListItem) => {
+          return <div>
+            <div>
+              <span>{JSON.parse(item.FuneralHome)?.DisplayName}</span>
+            </div>
+            <span>{JSON.parse(item.FuneralHome)?.Address?.Street}, {JSON.parse(item.FuneralHome)?.Address?.City}, {JSON.parse(item.FuneralHome)?.Address?.State} {JSON.parse(item.FuneralHome)?.Address?.PostalCode}</span>
+          </div>;
+        }
+      }
+    ];
+
+    const DETAIL_LIST_STILL_BIRTH_COLUMNS: IColumn[] = [
+      {
+        key: 'column1',
+        name: 'Child',
+        fieldName: 'Title',
+        minWidth: 30,
+        maxWidth: 100,
+        isResizable: true,
+      },
+      {
+        key: 'column2',
+        name: 'Still Birth Date',
+        fieldName: 'DateOfDeath',
+        minWidth: 30,
+        isResizable: true,
+        onRender: (item: IStillAndDeathRegisterListItem) => <span>{new Date(item.DateOfDeath).toLocaleDateString()}</span>
+      },
+      {
+        key: 'column3',
+        name: 'Reg Date',
+        fieldName: 'RegistrationDate',
+        minWidth: 30,
+        isResizable: true,
+        onRender: (item: IStillAndDeathRegisterListItem) => <span>{new Date(item.RegistrationDate).toLocaleDateString()}</span>
+      },
+      {
+        key: 'column4',
+        name: 'Reg Num',
+        fieldName: 'RegistrationNumber',
+        minWidth: 30,
+        isResizable: true,
+      },
+      {
+        key: 'column5',
+        name: 'Sex',
+        fieldName: 'Sex',
+        minWidth: 30,
+        isResizable: true,
+      },
+      {
+        key: 'column6',
+        name: 'Location',
+        fieldName: 'DeathLocation',
+        minWidth: 30,
+        isResizable: true,
+      },
+      {
+        key: 'column8',
+        name: "Doctor",
+        fieldName: "DoctorsInformation",
+        minWidth: 30,
+        isResizable: true,
+      },
+      {
+        key: 'column9',
+        name: "Informant",
+        fieldName: "InformantsInformation",
+        minWidth: 30,
+        isResizable: true,
+      },
+      {
+        key: 'column10',
+        name: "Funeral Home",
+        fieldName: "FuneralHome",
+        minWidth: 30,
+        isResizable: true,
+        onRender: (item: IStillAndDeathRegisterListItem) => {
+          return <div>
+            <div>
+              <span>{JSON.parse(item.FuneralHome)?.DisplayName}</span>
+            </div>
+            <span>{JSON.parse(item.FuneralHome)?.Address?.Street}, {JSON.parse(item.FuneralHome)?.Address?.City}, {JSON.parse(item.FuneralHome)?.Address?.State} {JSON.parse(item.FuneralHome)?.Address?.PostalCode}</span>
+          </div>;
+        }
+      }
+    ];
+
     return (
       <div>
         <h1>Registration Reports</h1>
@@ -37,7 +206,7 @@ export default class RegistrationReports extends React.Component<any, IRegistrat
                 label='From Registration Date'
                 id='fromDate'
                 // value={new Date()}
-                onSelectDate={(date: Date) => { this.setState({ fromDate: date }); }}
+                onSelectDate={(date: Date) => { this.setState({ fromDate: date, itemsFound: [] }); }}
               />
             </Stack.Item>
             <Stack.Item grow={3}>
@@ -47,7 +216,7 @@ export default class RegistrationReports extends React.Component<any, IRegistrat
                 label='To Registration Date'
                 id='toDate'
                 // value={new Date()}
-                onSelectDate={(date: Date) => { this.setState({ toDate: date }); }}
+                onSelectDate={(date: Date) => { this.setState({ toDate: date, itemsFound: [] }); }}
               />
             </Stack.Item>
             <Stack.Item grow={3}>
@@ -56,7 +225,7 @@ export default class RegistrationReports extends React.Component<any, IRegistrat
                 label="Select Report"
                 id='selectedReport'
                 options={[{ key: VitalStatsContentTypeIDs.DeathRegistration, text: 'Death Report' }, { key: VitalStatsContentTypeIDs.StillBirth, text: 'Still Birth Report' }]}
-                onChange={(event, option: IDropdownOption) => { this.setState({ selectedReport: option.key as VitalStatsContentTypeIDs }); }}
+                onChange={(event, option: IDropdownOption) => { this.setState({ selectedReport: option.key as VitalStatsContentTypeIDs, itemsFound: [] }); }}
               />
             </Stack.Item>
           </Stack>
@@ -79,30 +248,18 @@ export default class RegistrationReports extends React.Component<any, IRegistrat
           />
         </div>
         <div>
-          {/* <DetailsList
-            items={items}
-            compact={isCompactMode}
-            columns={columns}
-            selectionMode={SelectionMode.multiple}
-            setKey="multiple"
-            layoutMode={DetailsListLayoutMode.justified}
-            isHeaderVisible={true}
-            selection={this._selection}
-            selectionPreservedOnEmptyClick={true}
-            onItemInvoked={this._onItemInvoked}
-            enterModalSelectionOnTouch={true}
-            ariaLabelForSelectionColumn="Toggle selection"
-            ariaLabelForSelectAllCheckbox="Toggle selection for all items"
-            checkButtonAriaLabel="select row"
-          /> */}
-        </div>
-        <div>
           {
-            this.state?.itemsFound && this.state.itemsFound
-              .forEach((value: IStillAndDeathRegisterListItem) => {
-                return <div>{JSON.stringify(value)}<hr /></div>;
-              })
+            this.state?.itemsFound?.length > 0 &&
+            <DetailsList
+              items={this.state?.itemsFound}
+              compact={true}
+              columns={this.state.selectedReport === VitalStatsContentTypeIDs.DeathRegistration ? DETAIL_LIST_DEATH_REG_COLUMNS : DETAIL_LIST_STILL_BIRTH_COLUMNS}
+              selectionMode={SelectionMode.none}
+            />
           }
+          <div>
+            Date Printed: {new Date().toLocaleDateString()}
+          </div>
         </div>
         <hr />
         <PackageSolutionVersion />
