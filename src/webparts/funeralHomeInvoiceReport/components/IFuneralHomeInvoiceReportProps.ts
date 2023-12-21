8060e0049bb4f042d79773c652980ad4016e0f12
Steven Chorkawy <schorkawy@clarington.net>
2023-12-21 14:25:22 -0500

    Scaffolding new webpart for funeral home invoice report.

---
+++ src/webparts/funeralHomeInvoiceReport/components/IFuneralHomeInvoiceReportProps.ts
@@ -0,0 +1,7 @@
+export interface IFuneralHomeInvoiceReportProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
