28a64e01ef5035ebcc6af345d09feefe711566a8
Steven Chorkawy <schorkawy@clarington.net>
2023-12-21 12:08:17 -0500

    Scaffolding new monthly report webpart.

---
+++ src/webparts/monthlyDeathReports/components/IMonthlyDeathReportsProps.ts
@@ -0,0 +1,7 @@
+export interface IMonthlyDeathReportsProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
